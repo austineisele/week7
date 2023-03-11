3adcf254fb899f31623161d02afca4a0af7cb26a
austineisele <austineisele@gmail.com>
2023-03-11 07:13:54 -0500

    initial commit of files

---
+++ src/main/java/com/leszko/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.leszko.calculator;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/*********
+ *Calculator class
+ * ******/
+@Service
+public class Calculator {
+        final static int UMLNUMBER1 = 3;
+	@Cacheable("sum")
+	public int sum(int a, int b) {
+		return a + b;
+	}
+}
